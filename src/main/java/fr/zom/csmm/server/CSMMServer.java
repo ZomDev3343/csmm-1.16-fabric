67712c269ce32301df57bc56fc12237aadd91e4e
ZomDev3343 <contact.lefourretout@gmail.com>
2020-08-31 14:53:17 +0200

    - Add: Proxy Server
    #Episode 1

---
+++ src/main/java/fr/zom/csmm/server/CSMMServer.java
@@ -0,0 +1,12 @@
+package fr.zom.csmm.server;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class CSMMServer implements DedicatedServerModInitializer
+{
+    @Override
+    public void onInitializeServer()
+    {
+
+    }
+}
